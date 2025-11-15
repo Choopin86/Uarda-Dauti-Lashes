a2fb81280ef0b4d9cdd95e8dc4c4ed4813e648bb
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-11-15 12:52:40 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+import "./services.js";
+import { services } from "./services.js";
+const card = document.querySelector(".cards");
+
+function createCard() {
+  services.forEach((service) => {
+    const title = document.createElement("h2");
+    title.textContent = service.name;
+    const description = document.createElement("p");
+    description.textContent = service.description;
+    const price = document.createElement("h3");
+    price.textContent = service.price;
+    card.append(title, description, price);
+  });
+}
---
+++ src/services.js
@@ -0,0 +1,20 @@
+export const services = [
+  {
+    name: "Classic Eye Lashes",
+    description: `The length and the volume of this model is very similar to the natural look of you own lashes.
+      This model is for those girls who do not want to look extravagant.`,
+    price: 35,
+  },
+  {
+    name: "3D Eye Lashes",
+    description: `This model provides a fuller look to your natural lashes.
+      It is perfect for those who want to enhance their natural beauty with a more voluminous appearance.`,
+    price: 50,
+  },
+  {
+    name: "Volume Eye Lashes",
+    description: `This model offers a dramatic and glamorous look.
+      It is ideal for special occasions or for those who love to make a bold statement with their lashes.`,
+    price: 70,
+  },
+];
